ebd580a830fe7ce900b579940a7cf923de0d7c47
千啾略 <redwinam@gmail.com>
2024-05-31 02:54:53 +0000

    package, vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,33 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
   experimental: {
-    renderBuiltUrl(filename: string, { hostId, hostType, type }: { hostId: string, hostType: 'js' | 'css' | 'html', type: 'public' | 'asset' }) {
-      if (type === 'public') {
-        return 'https://cdn.jys-wtf.proxy.mayq.me/' + filename
+    renderBuiltUrl(filename: string, { hostId, hostType, type }: { hostId: string; hostType: "js" | "css" | "html"; type: "public" | "asset" }) {
+      if (type === "public") {
+        return "https://cdn.jys-wtf.proxy.mayq.me/" + filename;
+      } else if (path.extname(hostId) === ".js") {
+        return { runtime: `window.__assetsPath(${JSON.stringify(filename)})` };
+      } else {
+        return "https://cdn.jys-wtf.proxy.mayq.me/" + filename;
       }
-      else if (path.extname(hostId) === '.js') {
-        return { runtime: `window.__assetsPath(${JSON.stringify(filename)})` }
-      }
-      else {
-        return 'https://cdn.jys-wtf.proxy.mayq.me/' + filename
-      }
-    }
+    },
   },
   server: {
     proxy: {
-        '/api': {
-            target: 'http://47.100.10.147:9147',   //实际请求地址
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, '')
-        },
-    }
-  }
-})
+      "/api": {
+        target: "http://47.100.10.147:9147", //实际请求地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
