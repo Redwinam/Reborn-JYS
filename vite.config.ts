b586bda9c6e55251f8d2a2bf8a5f06eafad889b3
千啾略 <redwinam@gmail.com>
2025-01-10 00:13:38 +0800

    Refactor (vite.config): Comment out experimental URL rendering logic and maintain existing API proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,21 +10,21 @@ export default defineConfig({
       "@": "/src",
     },
   },
-  experimental: {
-    renderBuiltUrl(filename: string, { hostId, hostType, type }: { hostId: string; hostType: "js" | "css" | "html"; type: "public" | "asset" }) {
-      if (type === "public") {
-        return "https://cdn.jys-wtf.proxy.mayq.me/" + filename;
-      } else if (path.extname(hostId) === ".js") {
-        return { runtime: `window.__assetsPath(${JSON.stringify(filename)})` };
-      } else {
-        return "https://cdn.jys-wtf.proxy.mayq.me/" + filename;
-      }
-    },
-  },
+  // experimental: {
+  //   renderBuiltUrl(filename: string, { hostId, hostType, type }: { hostId: string; hostType: "js" | "css" | "html"; type: "public" | "asset" }) {
+  //     if (type === "public") {
+  //       return "https://cdn.jys-wtf.proxy.mayq.me/" + filename;
+  //     } else if (path.extname(hostId) === ".js") {
+  //       return { runtime: `window.__assetsPath(${JSON.stringify(filename)})` };
+  //     } else {
+  //       return "https://cdn.jys-wtf.proxy.mayq.me/" + filename;
+  //     }
+  //   },
+  // },
   server: {
     proxy: {
       "/api": {
-        target: "http://47.100.10.147:9147", //实际请求地址
+        target: "http://47.100.10.147:9147",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
